f57cc42ef1255e2cc3388787655605980a3ab520
Gerhard Stenzel <gerhard.stenzel@de.ibm.com>
2021-08-23 17:27:51 +0200

    remove plot

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 from qiskit import *
 from math import pi
 from qiskit.visualization import plot_bloch_multivector
@@ -5,11 +6,11 @@ from qiskit.visualization import plot_bloch_multivector
 # Let's do an X-gate on a |0> qubit
 qc = QuantumCircuit(1)
 qc.x(0)
-qc.draw()
+#qc.draw()
 
 # Let's see the result
 backend = Aer.get_backend('statevector_simulator')
 out = execute(qc,backend).result().get_statevector()
 print(out)
-plot_bloch_multivector(out)
+#plot_bloch_multivector(out)
 

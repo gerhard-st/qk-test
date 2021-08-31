200e2d7968d9f6de66c6a1300597966f178eef5c
Gerhard Stenzel <gerhard.stenzel@de.ibm.com>
2021-08-31 10:40:23 +0200

    output

--- test.py
+++ test.py
@@ -11,14 +11,22 @@ qc = QuantumCircuit(1,1)
 #
 qc.x(0)
 out = execute(qc,backend).result().get_statevector()
+print(qc)
 print(out)
 
 #
 qc.h(0)
 out = execute(qc,backend).result().get_statevector()
+print(qc)
 print(out)
 
 #
 qc.barrier()
 qc.measure(0,0)
 print(qc)
+print(execute(qc,backend).result().get_statevector())
+
+job = backend.run(qc, shots=1, memory=True)
+output  = job.result().get_memory()[0]
+#print(job.result())
+print(output)
